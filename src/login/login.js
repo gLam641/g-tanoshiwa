dd455781e6c656faca1bfabf86d12c9bc75d30ec
gLam641 <lam_gary@hotmail.com>
2021-02-07 19:52:29 -0500

    Added user registration, login, logout

---
+++ src/login/login.js
@@ -0,0 +1,138 @@
+import { useState } from 'react';
+import { Grid, Button } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+import React from 'react';
+
+axios.defaults.withCredentials = true;
+
+const Alert = React.forwardRef((props, ref) => {
+    return <MuiAlert ref={ref} elevation={6} variant="filled" {...props} />;
+});
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        padding: theme.spacing(4),
+    },
+    formClass: {
+        padding: theme.spacing(4),
+        width: "100%",
+        '& .MuiTextField-root': {
+            margin: theme.spacing(1),
+            width: "100%",
+        },
+    },
+    submitClass: {
+        marginTop: '2em',
+        marginLeft: '8px'
+    }
+}));
+
+export default function Login({ setUser }) {
+    const [snackBarMessage, setSnackBarMessage] = useState("");
+    const [snackBarSeverity, setSnackBarSeverity] = useState("success");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const classes = useStyles();
+    const history = useHistory();
+
+    const onFieldChange = (ev, setField) => {
+        const newFieldVal = ev.target.value;
+        if (newFieldVal && newFieldVal !== "") {
+            setField(newFieldVal);
+        } else {
+            setField("");
+        }
+    };
+
+
+    const isFormValid = () => {
+        return email && email !== "" &&
+            password && password !== "";
+    };
+
+    const onSubmit = (ev) => {
+        ev.preventDefault();
+
+        if (isFormValid()) {
+            axios.post('http://localhost:5000/user/login', {
+                email,
+                password
+            }).then((resp) => {
+                if (resp.status === 200) {
+                    setSnackBarMessage(`You have successfully logged in!`);
+                    setSnackBarSeverity("success");
+                    sessionStorage.setItem('session_id', resp.data.user.session_id);
+                    setUser(resp.data.user);
+                    setTimeout(() => {
+                        history.push('/');
+                    }, 1000);
+                }
+            }).catch((err) => {
+                setSnackBarMessage(`Login failed: ${err.response.data.msg.message}`);
+                setSnackBarSeverity("error");
+            });
+        } else {
+            setSnackBarMessage("Please fill in the missing fields.");
+            setSnackBarSeverity("warning");
+        }
+    };
+
+    const closeSnackbar = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setSnackBarMessage("");
+    };
+
+    return (
+        <Grid container className={classes.root}>
+            <Snackbar open={snackBarMessage !== ""} autoHideDuration={6000} onClose={closeSnackbar}>
+                <Alert onClose={closeSnackbar} severity={snackBarSeverity}>
+                    {snackBarMessage}
+                </Alert>
+            </Snackbar>
+            <Grid item xs={12}>
+                <Typography variant="h1">Login</Typography>
+            </Grid>
+            <Grid container item justify="center">
+                <form
+                    className={classes.formClass} noValidate autoComplete="off">
+                    <div>
+                        <TextField
+                            id="email"
+                            label="Email"
+                            error={!email}
+                            onChange={(ev) => { onFieldChange(ev, setEmail) }}
+                            variant="outlined"
+                            required
+                        />
+                        <TextField
+                            id="password"
+                            label="Password"
+                            error={!password}
+                            onChange={(ev) => { onFieldChange(ev, setPassword) }}
+                            variant="outlined"
+                            required
+                        />
+                        <Button
+                            className={classes.submitClass}
+                            color="primary"
+                            variant="contained"
+                            type="submit"
+                            onClick={onSubmit}
+                        >Submit</Button>
+                    </div>
+                </form>
+            </Grid>
+        </Grid>
+    );
+};
---
+++ src/register/register.js
@@ -0,0 +1,188 @@
+import { useState, useEffect } from 'react';
+import { Grid, Button } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+import React from 'react';
+
+const Alert = React.forwardRef((props, ref) => {
+    return <MuiAlert ref={ref} elevation={6} variant="filled" {...props} />;
+});
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        padding: theme.spacing(4),
+    },
+    formClass: {
+        padding: theme.spacing(4),
+        width: "100%",
+        '& .MuiTextField-root': {
+            margin: theme.spacing(1),
+            width: "100%",
+        },
+    },
+    submitClass: {
+        marginTop: '2em',
+        marginLeft: '8px'
+    }
+}));
+
+export default function Register() {
+    const [snackBarMessage, setSnackBarMessage] = useState("");
+    const [snackBarSeverity, setSnackBarSeverity] = useState("success");
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [isPasswordValid, setIsPasswordValid] = useState(false);
+    const [passwordHelper, setPasswordHelper] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [isConfirmPasswordValid, setIsConfirmPasswordValid] = useState(false);
+    const [confirmPasswordHelper, setConfirmPasswordHelper] = useState("");
+
+    const classes = useStyles();
+    const history = useHistory();
+
+    const onFieldChange = (ev, setField) => {
+        const newFieldVal = ev.target.value;
+        if (newFieldVal && newFieldVal !== "") {
+            setField(newFieldVal);
+        } else {
+            setField("");
+        }
+    };
+
+    useEffect(() => {
+        setPasswordHelper("");
+        setConfirmPasswordHelper("");
+        if ((!password || password === "") &&
+            (!confirmPassword || confirmPassword === "")) {
+            setIsPasswordValid(false);
+            setIsConfirmPasswordValid(false);
+        } else if (password.length < 6 || confirmPassword.length < 6) {
+            if (password.length < 6) {
+                setIsPasswordValid(false);
+                setPasswordHelper("Password must have at least 6 characters");
+            }
+            if (confirmPassword.length < 6) {
+                setIsConfirmPasswordValid(false);
+                setConfirmPasswordHelper("Password must have at least 6 characters");
+            }
+        } else if (password !== confirmPassword) {
+            setIsPasswordValid(false);
+            setIsConfirmPasswordValid(false);
+            setPasswordHelper("Passwords do not match");
+            setConfirmPasswordHelper("Passwords do not match");
+        } else {
+            setIsPasswordValid(true);
+            setIsConfirmPasswordValid(true);
+        }
+    }, [password, confirmPassword]);
+
+    const isFormValid = () => {
+        return email && email !== "" &&
+            password && password !== "" &&
+            confirmPassword && confirmPassword !== "" &&
+            password === confirmPassword;
+    };
+
+    const onSubmit = (ev) => {
+        ev.preventDefault();
+
+        if (isFormValid()) {
+            axios.post('http://localhost:5000/user/register', {
+                name,
+                email,
+                password,
+                confirmPassword,
+            }).then((resp) => {
+                if (resp.status === 200) {
+                    setSnackBarMessage(`You have successfully registered! Welcome ${name}`);
+                    setSnackBarSeverity("success");
+                    setTimeout(() => {
+                        history.push('/');
+                    }, 2000);
+                }
+            }).catch((err) => {
+                setSnackBarMessage(`Registration failed: ${err.response.data.errors[0].msg}`);
+                setSnackBarSeverity("error");
+            });
+        } else {
+            setSnackBarMessage("Please fill in the missing fields.");
+            setSnackBarSeverity("warning");
+        }
+    };
+
+    const closeSnackbar = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setSnackBarMessage("");
+    };
+
+    return (
+        <Grid container className={classes.root}>
+            <Snackbar open={snackBarMessage !== ""} autoHideDuration={6000} onClose={closeSnackbar}>
+                <Alert onClose={closeSnackbar} severity={snackBarSeverity}>
+                    {snackBarMessage}
+                </Alert>
+            </Snackbar>
+            <Grid item xs={12}>
+                <Typography variant="h1">Register</Typography>
+            </Grid>
+            <Grid container item justify="center">
+                <form
+                    className={classes.formClass} noValidate autoComplete="off">
+                    <div>
+                        <TextField
+                            id="name"
+                            label="Name"
+                            error={!name}
+                            onChange={(ev) => { onFieldChange(ev, setName) }}
+                            variant="outlined"
+                            required
+                        />
+                        <TextField
+                            id="email"
+                            label="Email"
+                            error={!email}
+                            onChange={(ev) => { onFieldChange(ev, setEmail) }}
+                            variant="outlined"
+                            required
+                        />
+                        <TextField
+                            id="password"
+                            label="Password"
+                            error={!isPasswordValid}
+                            onChange={(ev) => { onFieldChange(ev, setPassword) }}
+                            variant="outlined"
+                            helperText={passwordHelper}
+                            required
+                        />
+                        <TextField
+                            id="confirmPassword"
+                            label="Confirm Password"
+                            error={!isConfirmPasswordValid}
+                            onChange={(ev) => { onFieldChange(ev, setConfirmPassword) }}
+                            variant="outlined"
+                            helperText={confirmPasswordHelper}
+                            required
+                        />
+                        <Button
+                            className={classes.submitClass}
+                            color="primary"
+                            variant="contained"
+                            type="submit"
+                            onClick={onSubmit}
+                        >Submit</Button>
+                    </div>
+                </form>
+            </Grid>
+        </Grid>
+    );
+};
